9949095e0c5fea96ae529ad7db5f7644f25513be
Mrmulalo <mrmulalonetshiavha@gmail.com>
2023-03-17 23:33:37 +0200

    Advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-
+#include <string.h>
 /**
  * main - Entry point of the program
  * Description: Prints a message to the standard error
@@ -7,10 +7,14 @@
  */
 int main(void)
 {
-	const char msg[];
+	const char msg[] = "and that piece of art is useful\" - ";
+	const char add[] = "Dora Korpar, 2015-10-19\n";
+	char new_msg[sizeof(msg) + sizeof(add)];
 
-	msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, msg, sizeof(msg) - 1);
+	strcpy(new_msg, msg);
+	strcat(new_msg, add);
+
+	write(STDERR_FILENO, new_msg, sizeof(new_msg) - 1);
 	return (1);
 }
 
