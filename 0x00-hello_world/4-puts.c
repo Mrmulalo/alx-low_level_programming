e217b757900b0df5648dda894602ddf07cfd6ade
Mrmulalo <mrmulalonetshiavha@gmail.com>
2023-03-17 02:01:14 +0200

    C programming 2 to 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: Prints a string using printf function
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	    puts("Programming is like building a multilingual puzzle,");
-	    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
