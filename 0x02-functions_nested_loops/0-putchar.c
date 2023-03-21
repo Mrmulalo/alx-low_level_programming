4df4816bb0196b807b6ad4062e0c3e75be3f84b8
Mrmulalo <mrmulalonetshiavha@gmail.com>
2023-03-22 01:11:17 +0200

    0-putchar and alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -10,8 +10,8 @@ int main(void)
 	char mystring[] = "_putchar";
 
 	for (i = 0; i < 8; i++)
-		putchar(mystring[i]);
-	putchar('\n');
+		_putchar(mystring[i]);
+	_putchar('\n');
 
 	return (0);
 }
