fbe60ef9729c396d22b91c216575a38a793f133d
Mrmulalo <mrmulalonetshiavha@gmail.com>
2023-03-23 00:41:02 +0200

    9 to 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result.
+ * @a: variable to be added.
+ * @b: variable to be added.
+ * Return: sum of two integers
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98.
+ * @n: starting point.
+ * Return: void
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i  = n; i <= 98; i++)
+		{
+			_putchar(i + '0');
+			if (i  != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for (i  = n; i >= 98; i--)
+		{
+			_putchar(i + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
