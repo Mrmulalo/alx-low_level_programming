cef07702aab3090232b31ee10ee53857e9446e7a
Mrmulalo <mrmulalonetshiavha@gmail.com>
2023-03-23 22:54:33 +0200

    modified print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,7 +18,7 @@ void print_to_98(int n)
 			if (i  != 98)
 				printf(", ");
 		}
-		_putchar('\n');
+		printf("\n");
 	}
 	else
 	{
@@ -28,6 +28,6 @@ void print_to_98(int n)
 			if (i != 98)
 				printf(", ");
 		}
-		_putchar('\n');
+		printf("\n");
 	}
 }
