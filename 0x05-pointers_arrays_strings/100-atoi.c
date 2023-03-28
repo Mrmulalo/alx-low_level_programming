7f0d6c490de1df1611c52e7410eaffe7a5278eb0
Mrmulalo <mrmulalonetshiavha@gmail.com>
2023-03-29 00:32:32 +0200

    advanced task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <limits.h>
+/**
+ * _atoi - converts sting to an integer.
+ * @s: intput string.
+ * Return: an integer.
+ */
+int _atoi(char *s)
+{
+	int i;
+	int sign;
+	int num;
+
+	sign = 1;
+	num = 0;
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		else if (s[i] == '+')
+			continue;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + (s[i] + '0');
+			if (num * sign < INT_MIN)
+				return (0);
+			if (num * sign > INT_MAX)
+				return (0);
+		}
+		else if (num > 0)
+			break;
+		i++;
+	}
+	return (num * sign);
+}
