ab87dc8c3ddddc4dc75d5e72905f10fa02e482e3
Mrmulalo <mrmulalonetshiavha@gmail.com>
2023-03-29 00:49:47 +0200

    advanced task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,12 +19,9 @@ int _atoi(char *s)
 	{
 		if (s[i] == '-')
 			sign *= -1;
-
-		else if (s[i] == '+')
-			continue;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			num = num * 10 + (s[i] + '0');
+			num = num * 10 + (s[i] - '0');
 			if (num * sign < INT_MIN)
 				return (0);
 			if (num * sign > INT_MAX)
